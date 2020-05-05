834a130a938097f472a0da3ad88057efcd0ff653
abduallahnoorsayket <abdullahnoorsayket@gmail.com>
2020-05-05 13:07:54 +0600

    this is new commit

--- myApp/views.py
+++ myApp/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 def index(request):
-    return HttpResponse(" Django on Docker on AWS  . salam world!")
+    return HttpResponse(" Hey Docker why you hate windows So much ?! . salam world! \n hello !  i am abdullah noor sayket ")
